874de5b9aa30741cafeba6762ca628a92989a881
Changsik Yoon <dbs2636@gmail.com>
2021-11-27 18:32:32 +0900

    feat: Add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import Responsive from './Responsive';
+
+const HeaderBlock = styled.header`
+  position: fixed;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Spacer = styled.div`
+  height: 4rem;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <h2>
+            <Link to="/">쇼미더럭키밈</Link>
+          </h2>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+};
+
+export default Header;
