27f789b093bbcbcdb094d4b338664dfc121e52bf
Changsik Yoon <dbs2636@gmail.com>
2021-12-04 22:36:30 +0900

    feat: Add meta tag container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MetaContainer from './containers/MetaContainer';
 import IndexPage from './pages/IndexPage';
 
 const App = () => {
   return (
     <>
+      <MetaContainer />
       <Routes>
         <Route path="/" element={<IndexPage />} />
       </Routes>
---
+++ src/containers/MetaContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const MetaContainer = () => {
+  return (
+    <Helmet>
+      <title>쇼미더럭키밈</title>
+      <meta property="og:title" content="쇼미더럭키밈" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content="쇼미더럭키밈 - 짤 생성기" />
+      <meta property="og:image" content="/images/crop.png" />
+      <meta property="og:image:alt" content="말대꾸 짤" />
+      <meta property="og:image:width" content="848" />
+      <meta property="og:image:height" content="561" />
+    </Helmet>
+  );
+};
+
+export default MetaContainer;
