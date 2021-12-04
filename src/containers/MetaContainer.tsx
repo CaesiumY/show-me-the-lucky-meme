e1b40ff5e52089f56df2d5bbf9fdd61941defda2
Changsik Yoon <dbs2636@gmail.com>
2021-12-05 00:08:19 +0900

    feat: Update metas with og generator

--- src/containers/MetaContainer.tsx
+++ src/containers/MetaContainer.tsx
@@ -5,21 +5,33 @@ const MetaContainer = () => {
   return (
     <Helmet>
       <title>쇼미더럭키밈</title>
-      <meta property="og:title" content="쇼미더럭키밈" />
-      <meta property="og:type" content="website" />
-      <meta property="og:description" content="쇼미더럭키밈 - 짤 생성기" />
-      <meta property="og:image" content="/images/crop.png" />
-      <meta property="og:image:alt" content="말대꾸 짤" />
-      <meta property="og:image:width" content="848" />
-      <meta property="og:image:height" content="561" />
-      <meta name="twitter:title" content="쇼미더럭키밈" />
-      <meta name="twitter:type" content="website" />
-      <meta name="twitter:description" content="쇼미더럭키밈 - 짤 생성기" />
-      <meta name="twitter:image" content="/images/crop.png" />
-      <meta name="twitter:image:alt" content="말대꾸 짤" />
-      <meta name="twitter:image:width" content="848" />
-      <meta name="twitter:image:height" content="561" />
       <meta name="description" content="쇼미더럭키밈 - 짤 생성기" />
+      <meta
+        property="og:url"
+        content="https://show-me-the-lucky-meme.vercel.app/"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="쇼미더럭키밈" />
+      <meta property="og:description" content="쇼미더럭키밈 - 짤 생성기" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/CaesiumY/show-me-the-lucky-meme/main/public/images/crop.png"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:domain"
+        content="show-me-the-lucky-meme.vercel.app"
+      />
+      <meta
+        property="twitter:url"
+        content="https://show-me-the-lucky-meme.vercel.app/"
+      />
+      <meta name="twitter:title" content="쇼미더럭키밈" />
+      <meta name="twitter:description" content="쇼미더럭키밈 - 짤 생성기" />
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/CaesiumY/show-me-the-lucky-meme/main/public/images/crop.png"
+      />
     </Helmet>
   );
 };
